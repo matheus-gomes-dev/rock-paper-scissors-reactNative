c765dcd89d9bb5a5db716424f6b0c90eaf4c5245
matheus-lima92 <lima.eca@gmail.com>
2018-04-22 23:53:08 -0300

    initial reactNative structure

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+
+export default class App extends React.Component {
+
+  //const { container } = Styles
+
+  render() {
+    return (
+      <Text>New App</Text>
+    );
+  }
+}
+
+
