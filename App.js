a2184f4ac78f98d30f95b048fa5443bee45bbe08
matheus-lima92 <lima.eca@gmail.com>
2018-04-23 12:48:19 -0300

    initial logic implementation

--- App.js
+++ App.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 
 export default class App extends React.Component {
 
-  //const { container } = Styles
+  	constructor(props){
+		super(props);
+  		this.state = {userChoice: ''};
+  	}
 
-  render() {
-    return (
-      <Text>New App</Text>
-    );
-  }
+  	game(userChoice){
+  		this.setState({userChoice: userChoice});
+  	}
+
+  	render() {
+	    return (
+	    	<View>
+		      	<Text>Computer's choice:</Text>
+	    	  	<Text>User's choice: {this.state.userChoice}</Text>
+	      		<Text>Result:</Text>
+	      		<Button title='rock' onPress={ () => this.game('rock') }></Button>
+	      		<Button title='paper' onPress={ () => this.game('paper') }></Button>
+	      		<Button title='scissors' onPress={ () => this.game('scissors') }></Button>
+	      	</View>
+	    );
+  	}
 }
 
 
